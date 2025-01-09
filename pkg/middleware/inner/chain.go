9cb34080451ff3ffb5cd619b20c3dc9063c8af17
Peter Gillich <pgillich@gmail.com>
2025-01-09 17:35:02 +0100

    InternalMiddlewareChain CastResult

--- pkg/middleware/inner/chain.go
+++ pkg/middleware/inner/chain.go
@@ -3,11 +3,16 @@ package inner
 
 import (
 	"context"
+	"errors"
 )
 
 type InternalMiddlewareFn func(ctx context.Context) (interface{}, error)
 
 type InternalMiddleware func(next InternalMiddlewareFn) InternalMiddlewareFn
+
+var (
+	ErrUnableToCast = errors.New("interface cast error")
+)
 
 func InternalMiddlewareChain(mws ...InternalMiddleware) InternalMiddleware {
 	return func(next InternalMiddlewareFn) InternalMiddlewareFn {
@@ -18,4 +23,16 @@ func InternalMiddlewareChain(mws ...InternalMiddleware) InternalMiddleware {
 
 		return fn
 	}
+}
+
+func CastResult[T any](result interface{}, err error) (T, error) {
+	var typedErr error
+	typedResult, is := result.(T)
+	if !is {
+		typedErr = ErrUnableToCast
+	}
+	if err == nil {
+		err = typedErr
+	}
+	return typedResult, err
 }
